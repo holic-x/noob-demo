b0a0afad73c9ab86fef9dacfcce8b398084de6d3
hahabibu <43883789+hahabibu@users.noreply.github.com>
2021-06-17 09:00:07 +0800

    springboot-demo-aop项目参考学习
    springboot-demo-aop项目参考学习

---
+++ springboot-demo-aop/src/test/java/com/sz/demo/SpringbootDemoAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoAopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
