d458be45c64dbadfa62e6651a3923dce4a79e363
holic-x <holic-x@foxmail.com>
2024-03-31 10:15:40 +0800

    反射机制
    反射机制

---
+++ java-demo/JavaReflection/src/main/java/reflection/User.java
@@ -0,0 +1,14 @@
+package reflection;
+
+/**
+ * 用户类
+ */
+public class User {
+
+    private String name;
+
+    public void callMyName(String name){
+        System.out.println("hello：" + name);
+    }
+
+}
