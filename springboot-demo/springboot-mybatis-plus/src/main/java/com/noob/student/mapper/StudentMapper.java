0f2cef3ddd67fc34ad73afae70f50ed89db225ad
holic-x <holic-x@foxmail.com>
2024-04-05 18:58:46 +0800

    springboot整合MyBatis-plus
    springboot整合MyBatis-plus

---
+++ springboot-demo/springboot-mybatis-plus/src/main/java/com/noob/student/mapper/StudentMapper.java
@@ -0,0 +1,18 @@
+package com.noob.student.mapper;
+
+import com.noob.student.domain.Student;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author hahabibu
+* @description 针对表【student】的数据库操作Mapper
+* @createDate 2024-04-05 17:42:31
+* @Entity com.noob.student.domain.Student
+*/
+public interface StudentMapper extends BaseMapper<Student> {
+
+}
+
+
+
+
