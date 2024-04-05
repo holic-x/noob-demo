6d176b1579d49e3a76ee2d6d6f0d2c7573def97c
holic-x <holic-x@foxmail.com>
2024-04-05 17:35:24 +0800

    springboot-myabtis
    springboot-myabtis

---
+++ springboot-demo/springboot_helloworld/src/main/java/cn/noob/pojo/Emp.java
@@ -0,0 +1,33 @@
+package cn.noob.pojo;
+
+public class Emp {
+    private Integer empid;
+
+    private String empno;
+
+    private String ename;
+
+    public Integer getEmpid() {
+        return empid;
+    }
+
+    public void setEmpid(Integer empid) {
+        this.empid = empid;
+    }
+
+    public String getEmpno() {
+        return empno;
+    }
+
+    public void setEmpno(String empno) {
+        this.empno = empno;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+}
