5352b867c0535b7ae46bc458fa23453410883559
Huh-x <97139830+Huh-x@users.noreply.github.com>
2024-04-25 15:39:17 +0800

    feat：maven聚合
    maven聚合

---
+++ maven-demo/maven-module-A/maven-module-D/src/main/java/com/noob/base/Main.java
@@ -0,0 +1,13 @@
+package com.noob.base;
+
+/**
+ * @ClassName Huh-x
+ * @Description TODO
+ * @Author Huh-x
+ * @Date 2024 2024/4/25 15:16
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
