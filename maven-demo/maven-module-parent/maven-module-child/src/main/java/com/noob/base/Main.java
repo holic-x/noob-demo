8a2acb357c73d08bed676adb1c57a86b12c22032
Huh-x <97139830+Huh-x@users.noreply.github.com>
2024-04-25 16:15:13 +0800

    feat：maven 继承案例
    maven 继承案例

---
+++ maven-demo/maven-module-parent/maven-module-child/src/main/java/com/noob/base/Main.java
@@ -0,0 +1,13 @@
+package com.noob.base;
+
+/**
+ * @ClassName Huh-x
+ * @Description TODO
+ * @Author Huh-x
+ * @Date 2024 2024/4/25 15:56
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
