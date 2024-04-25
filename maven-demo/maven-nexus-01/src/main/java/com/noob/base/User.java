d586d3d68cbf54c6b3b4de2da5ee3213b92aba1e
Huh-x <97139830+Huh-x@users.noreply.github.com>
2024-04-25 13:54:18 +0800

    refactor：Maven基础应用 搭建Nexus私服 测试构件发布
    Maven基础应用 搭建Nexus私服 测试构件发布

---
+++ maven-demo/maven-nexus-01/src/main/java/com/noob/base/User.java
@@ -0,0 +1,19 @@
+package com.noob.base;
+
+/**
+ * @ClassName Huh-x
+ * @Description TODO
+ * @Author Huh-x
+ * @Date 2024 2024/4/25 13:25
+ */
+public class User {
+
+    private String name;
+
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
