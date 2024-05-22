9dc7a837b4599b76eba21c4f5f4640106f9e6d71
Huh-x <97139830+Huh-x@users.noreply.github.com>
2024-05-22 11:43:55 +0800

    feat：SPI机制 单体应用
    feat：SPI机制 单体应用

---
+++ rebirth/base/JavaBase/src/main/java/com/noob/base/spi/DataStorage.java
@@ -0,0 +1,8 @@
+package com.noob.base.spi;
+
+/**
+ * 自定义Spi接口服务 DataStorage
+ */
+public interface DataStorage {
+    void search(String searchParam);
+}
---
+++ rebirth/base/JavaBase/src/main/java/com/noob/base/spi/SpiDemo.java
@@ -0,0 +1,33 @@
+package com.noob.base.spi;
+
+import com.noob.base.spi.impl.MysqlStorage;
+import com.noob.base.spi.impl.RedisStorage;
+
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
+/**
+ * Spi demo 测试（模拟业务模块）：动态加载实现类
+ */
+public class SpiDemo {
+    public static void main(String[] args) {
+        // 传统方式调用服务实现接口
+        System.out.println("1.传统方式调用服务实现接口");
+        MysqlStorage mysqlStorage = new MysqlStorage();
+        mysqlStorage.search("hello");
+        RedisStorage redisStorage = new RedisStorage();
+        redisStorage.search("hello");
+
+        // SPI机制下动态加载服务接口(从业务代码中解耦，实现动态插拔)
+        System.out.println("2.SPI机制下动态加载服务接口(从业务代码中解耦，实现动态插拔)");
+        // 使用ServiceLoader动态加载指定接口的实现类
+        ServiceLoader<DataStorage> loader = ServiceLoader.load(DataStorage.class);
+        // 使用迭代器迭代实现类信息
+        Iterator<DataStorage> iterator = loader.iterator();
+        while (iterator.hasNext()) {
+            DataStorage storage = iterator.next();
+            // 指定对应实现类指定的操作
+            storage.search("keep");
+        }
+    }
+}
