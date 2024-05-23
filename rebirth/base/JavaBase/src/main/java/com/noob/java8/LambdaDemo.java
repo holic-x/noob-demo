174c017554738c821113d1ab3d4985bb2101760f
Huh-x <97139830+Huh-x@users.noreply.github.com>
2024-05-23 18:44:27 +0800

    feat：Lambda表达式应用、函数式接口
    feat：Lambda表达式应用、函数式接口

---
+++ rebirth/base/JavaBase/src/main/java/com/noob/java8/LambdaDemo.java
@@ -0,0 +1,37 @@
+package com.noob.java8;
+
+interface MyInterface{
+    // 抽象方法
+    void doSth(String value);
+
+    // 默认方法
+    default void print(){
+        System.out.println("hello default print");
+    }
+
+    // 静态方法
+    static void show(){
+        System.out.println("hello static show");
+    }
+}
+
+/**
+ * Lambda表达式案例
+ */
+public class LambdaDemo {
+    public static void main(String[] args) {
+        // 1.传统方式实现接口
+        new MyInterface() {
+             @Override
+             public void doSth( String value) {
+                 System.out.println("传统方式 doSth:" + value);
+             }
+         }.doSth("冲冲冲");
+
+        // 2.Lambda表达式
+        MyInterface myInterface = (val) -> {
+            System.out.println("Lambda doSth:" + val);
+        };
+        myInterface.doSth("hello");
+    }
+}
