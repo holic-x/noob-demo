1d6e08461a266398c21061fbae94b019dfcb5a00
Huh-x <97139830+Huh-x@users.noreply.github.com>
2024-05-23 18:23:56 +0800

    feat：懒汉式单例模式（多线程版本优化：同步锁+双检）
    feat：懒汉式单例模式（多线程版本优化：同步锁+双检）

--- rebirth/base/JavaBase/src/main/java/com/noob/design/singleton/MultiSingletonDemo.java
+++ rebirth/base/JavaBase/src/main/java/com/noob/design/singleton/MultiSingletonDemo.java
@@ -1,23 +1,27 @@
 package com.noob.design.singleton;
 
 // 饱汉式|懒汉式(只有在要用到实例的时候才创建)
-class LazySingleton{
+class LazySingleton {
     // 1.自行创建实例（该实例私有化）
-    private static LazySingleton instance = null;
+    private volatile static LazySingleton instance = null;
 
     // 定义全局计数器，统计构造函数调用次数
-    private static int count ;
+    private static int count;
 
     // 2.构造函数私有化
-    private LazySingleton(){
+    private LazySingleton() {
         System.out.println("LazySingleton 构造函数被调用第" + (count++) + "次");
     }
 
     // 3.对外提供访问该实例的入口
-    public static LazySingleton getInstance(){
+    public static LazySingleton getInstance() {
         // 饱汉式是只有在用到实例的时候进行判断，如果实例为null则常见
-        if(instance==null){
-            instance = new LazySingleton();
+        if(instance == null) {
+            synchronized (LazySingleton.class) {
+                if (instance == null) {
+                    instance = new LazySingleton();
+                }
+            }
         }
         return instance;
     }
@@ -29,14 +33,14 @@ class LazySingleton{
 public class MultiSingletonDemo {
     public static void main(String[] args) {
         // 验证多线程场景下懒汉式的不安全性
-        Runnable task = ()->{
+        Runnable task = () -> {
             String threadName = Thread.currentThread().getName();
             LazySingleton instance = LazySingleton.getInstance();
             System.out.println("线程 " + threadName + "\t => " + instance.hashCode());
         };
         // 模拟多线程环境下使用 Singleton 类获得对象
-        for(int i=0;i<100;i++){
-            new Thread(task,"" + i).start();
+        for (int i = 0; i < 100; i++) {
+            new Thread(task, "" + i).start();
         }
     }
 }
