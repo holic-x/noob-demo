fd07f7ad143baba4b568158f34d6ae043b6639e7
Huh-x <97139830+Huh-x@users.noreply.github.com>
2024-05-24 21:02:32 +0800

    feat：集合框架 案例学习
    feat：集合框架 案例学习

---
+++ rebirth/base/JavaBase/src/main/java/com/noob/collection/HashMapDemo.java
@@ -0,0 +1,10 @@
+package com.noob.collection;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+
+    public static void main(String[] args) {
+        HashMap map = new HashMap();
+    }
+}
