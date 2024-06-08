320ca3a7fda94f59348e368945e4ab3616c46309
holic-x <holic-x@foxmail.com>
2024-06-08 20:47:46 +0800

    feat：springboot 多数据源
    feat：springboot 多数据源

---
+++ rebirth/framework/springboot-mybatis-multiDatasource/src/main/java/com/noob/framework/datasource/SpringbootMybatisMultiDatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.noob.framework.datasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisMultiDatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisMultiDatasourceApplication.class, args);
+    }
+
+}
---
+++ rebirth/framework/springboot-mybatis-multiDatasource/src/test/java/com/noob/framework/datasource/SpringbootMybatisMultiDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noob.framework.datasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisMultiDatasourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
