674723f3882b83ae3bbb02a7156f40e8d539b86f
holic-x <holic-x@foxmail.com>
2024-06-08 22:24:00 +0800

    feat：springboot retry demo
    feat：springboot retry demo

---
+++ rebirth/framework/springboot-retry-demo/src/main/java/com/noob/framework/retry/SpringbootRetryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.noob.framework.retry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRetryDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRetryDemoApplication.class, args);
+    }
+
+}
---
+++ rebirth/framework/springboot-retry-demo/src/test/java/com/noob/framework/retry/SpringbootRetryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noob.framework.retry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRetryDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
