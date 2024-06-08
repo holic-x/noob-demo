d3152c1b0ad3634a9fcd00bf89e092e571dcdb6c
holic-x <holic-x@foxmail.com>
2024-06-08 12:32:43 +0800

    fix：springboot-transaction 事务失效场景 解决方案（避免自调用）
    fix：springboot-transaction 事务失效场景 解决方案（避免自调用）

--- rebirth/framework/springboot-transaction/src/main/java/com/noob/framework/transaction/service/TransactionServiceC.java
+++ rebirth/framework/springboot-transaction/src/main/java/com/noob/framework/transaction/service/TransactionServiceC.java
@@ -2,7 +2,9 @@ package com.noob.framework.transaction.service;
 
 import com.noob.framework.transaction.entity.TableEntity;
 import com.noob.framework.transaction.mapper.TableMapper;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,13 +12,19 @@ import java.util.UUID;
 
 @Service
 public class TransactionServiceC {
+
     @Autowired
     private TableMapper tableMapper;
+
+    @Lazy
+    @Autowired
+    private TransactionServiceC transactionServiceC;
 
     // 内部方法
     public void methodC(){
         tableMapper.insertTableA(new TableEntity(UUID.randomUUID().toString().replaceAll("-","")));
-        add();
+        transactionServiceC.add();
+        // ((TransactionServiceC) AopContext.currentProxy()).add();
     }
 
     @Transactional
