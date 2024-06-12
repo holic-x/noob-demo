f4fb1c22e73e8b96a3739a03a40d5043b16de7f2
Huh-x <97139830+Huh-x@users.noreply.github.com>
2024-06-12 10:31:02 +0800

    feat：init springboot-base-demo
    feat：init springboot-base-demo

---
+++ rebirth/framework/springboot-base-demo/src/main/java/com/noob/framework/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.noob.framework.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "success";
+    }
+
+}
