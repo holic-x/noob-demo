fe0b4975e96cb2f354e00286ab21399e602c5021
Huh-x <97139830+Huh-x@users.noreply.github.com>
2024-06-13 16:50:38 +0800

    feat：Springboot 自定义redis starter
    feat：Springboot 自定义redis starter

--- rebirth/framework/springboot-starter-redis-demo/springboot-starter-test/src/main/java/com/noob/framework/controller/DemoController.java
+++ rebirth/framework/springboot-starter-redis-demo/springboot-starter-test/src/main/java/com/noob/framework/controller/DemoController.java
@@ -1,22 +1,28 @@
 package com.noob.framework.controller;
 
-import com.noob.framework.module.DemoModule;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
 
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
 
     @Autowired
-    private DemoModule demoModule;
+    private Jedis jedis;
 
-    @GetMapping("/execute")
-    public String execute(){
-        return demoModule.exeModuleMethod();
+    @GetMapping("/link")
+    public String link(){
+        // 测试获取redis
+        System.out.println(jedis);
+        // 测试jedis连接配置
+        jedis.set("hello","hello starter");
+        return "success:" + jedis.get("hello");
     }
 
 }
