6558077b5a5caf39066f295332c6531f2c0a17bb
holic-x <holic-x@foxmail.com>
2024-07-06 20:08:21 +0800

    fix：数据导出txt文本
    fix：数据导出txt文本

--- springboot-demo/springboot-demo-batchHandler/src/test/java/com/noob/base/springbootdemobatchhandler/GenDataTest.java
+++ springboot-demo/springboot-demo-batchHandler/src/test/java/com/noob/base/springbootdemobatchhandler/GenDataTest.java
@@ -20,7 +20,41 @@ class GenDataTest {
      * 模拟生成数据（生成SQL文件）
      */
     @Test
-    public void batchGenTUserBatch() throws Exception {
+    public void batchGenTUserBatchToTxt() throws Exception {
+        List<TUserBatch> tUserBatchList = RandomGenEntityUtil.genTUserBatch(1000000);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        // 封装SQL文本信息
+        List<String> insertList = new ArrayList<>();
+        String split = ",";
+        tUserBatchList.forEach(tUserBatch -> {
+            // 拼接SQL语句
+            StringBuffer insertSql = new StringBuffer();
+            // 主键自增
+            insertSql.append(tUserBatch.getAccount() + split)
+                    .append(tUserBatch.getMobile() + split)
+                    .append(sdf.format(tUserBatch.getAdd_time()));
+            insertList.add(insertSql.toString());
+        });
+
+        // 写入文件
+        FileWriter writer = new FileWriter(new File("D:\\Desktop\\tmp\\insert.txt"));
+        insertList.forEach(insertSql -> {
+            try {
+                writer.write(insertSql + "\n");
+            } catch (IOException e) {
+                System.out.println("数据写入异常" + insertSql);
+                throw new RuntimeException(e);
+            }
+        });
+        // 关闭写入流
+        writer.close();
+    }
+
+    /**
+     * 模拟生成数据（生成SQL文件）
+     */
+    @Test
+    public void batchGenTUserBatchToSQL() throws Exception {
         List<TUserBatch> tUserBatchList = RandomGenEntityUtil.genTUserBatch(1000000);
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         // 封装SQL文本信息
