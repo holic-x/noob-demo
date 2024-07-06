25d3abf20b3db15ef7fa8589903640aeeb20ed18
holic-x <holic-x@foxmail.com>
2024-07-06 10:08:50 +0800

    feat：init springboot-demo-batchHandle
    feat：init springboot-demo-batchHandle

---
+++ springboot-demo/springboot-demo-batchHandler/src/main/java/com/noob/base/springbootdemobatchhandler/SpringbootDemoBatchHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.noob.base.springbootdemobatchhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoBatchHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoBatchHandlerApplication.class, args);
+    }
+
+}
