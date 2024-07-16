71dbd7d2f8d67348934b71ba8f879368505f933c
holic-x <holic-x@foxmail.com>
2024-07-16 22:22:33 +0800

    feat：init springboot-demo-sharding01
    feat：init springboot-demo-sharding01

---
+++ springboot-demo/springboot-demo-sharding01/src/main/java/com/noob/base/SpringbootDemoWatermarkApplication.java
@@ -0,0 +1,13 @@
+package com.noob.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoWatermarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoWatermarkApplication.class, args);
+    }
+
+}
