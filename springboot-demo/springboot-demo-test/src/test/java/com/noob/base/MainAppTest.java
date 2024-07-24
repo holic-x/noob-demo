34370d8c910b2907e9c063dbe60fb45edfbcf621
holic-x <holic-x@foxmail.com>
2024-07-24 19:55:10 +0800

    fix：实体get、set ut覆盖率工具类
    fix：实体get、set ut覆盖率工具类

--- springboot-demo/springboot-demo-test/src/test/java/com/noob/base/MainAppTest.java
+++ springboot-demo/springboot-demo-test/src/test/java/com/noob/base/MainAppTest.java
@@ -9,9 +9,15 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * 测试类
+ * 注意覆盖率统计的范围
+ * 除却实体还有Enum枚举、普通异常等相关的代码需要进行覆盖
+ */
 class MainAppTest {
 
     /**
+     * 可覆盖Lombok注解构建的实体，但仅限与无参构造函数和getter、setter的调用（其他调用覆盖需额外测试或者补充）
      * 部分情况无法完全覆盖
      */
     @Test
@@ -34,6 +40,7 @@ class MainAppTest {
     }
 
     /**
+     * 测试工具类兼容性不足，仅限于普通实体的getter、setter覆盖
      * 存在不足（Lombok构建的实体无法覆盖）
      */
     @SneakyThrows
