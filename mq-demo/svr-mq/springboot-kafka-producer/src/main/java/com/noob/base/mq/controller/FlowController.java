f9781e766d5db1158f760ba4cdfffeab82996adf
Huh-x <97139830+Huh-x@users.noreply.github.com>
2024-08-21 14:53:36 +0800

    feat：MQ kafka 削峰填谷案例
    feat：MQ kafka 削峰填谷案例

--- mq-demo/svr-mq/springboot-kafka-producer/src/main/java/com/noob/base/mq/controller/FlowController.java
+++ mq-demo/svr-mq/springboot-kafka-producer/src/main/java/com/noob/base/mq/controller/FlowController.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.noob.base.mq.model.dto.IncrCountReq;
 import com.noob.base.mq.service.CountService;
+import com.noob.base.mq.service.FlowService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,32 +16,40 @@ import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.web.bind.annotation.*;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
-@RestController
-@RequestMapping("/kafka/producer")
 @Slf4j
-public class ProducerController {
+@RestController
+@RequestMapping("/flow")
+public class FlowController {
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
     @Autowired
-    CountService countService;
+    private FlowService flowService;
+
+    // 传统方式
+    @GetMapping(value = "/peakClipping")
+    public ResponseEntity<String> peakClipping() {
+        // 模拟调用下游服务
+        flowService.handler();
+        return ResponseEntity.ok("success");
+    }
 
     // 引入MQ：此处生产者做发送消息（执行逻辑由消费者实现）
-    @PostMapping(value = "/decouplingWithMQ",consumes = "application/json;charset=utf-8")
-    public ResponseEntity<String> decouplingWithMQ(@RequestBody IncrCountReq incrCountReq) {
+    @GetMapping(value = "/peakClippingWithMQ")
+    public ResponseEntity<String> peakClippingWithMQ() {
+        String msg = "hello kafka";
         // 传递消息，发送到kafka消息队列
-        kafkaTemplate.send("tp-mq-decoupling", JSON.toJSONString(incrCountReq));
+        kafkaTemplate.send("tp-mq-peakClipping", msg);
         return ResponseEntity.ok("success");
     }
 
     // 模拟消费者消费
-    // @KafkaListener(topics = "tp-mq-decoupling",groupId = "TEST_GROUP",concurrency = "1",containerFactory = "kafkaManualAckListenerContainerFactory")
-    @KafkaListener(topics = "tp-mq-decoupling",groupId = "TEST_GROUP") // 使用Springboot的KafkaListener注册一个kafka消费者
-    // public void topicConsumer(ConsumerRecord<?,?> record, Acknowledgment ack, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+    @KafkaListener(topics = "tp-mq-peakClipping",groupId = "TEST_GROUP") // 使用Springboot的KafkaListener注册一个kafka消费者
+//     public void topicConsumer(ConsumerRecord<?,?> record, Acknowledgment ack, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
     public void topicConsumer(ConsumerRecord<?,?> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
         Optional message = Optional.ofNullable(record.value());
         if(message.isPresent()){
@@ -49,7 +58,9 @@ public class ProducerController {
             log.info("接收到的kafka消息:{}",msg);
             try{
                 // 模拟消费消息
-                countService.incrManyTimes(JSONObject.parseObject(msg.toString(),IncrCountReq.class).getNum());
+                flowService.handler();
+                // 模拟沉睡1s（假设消费者每秒只能处理1条消息，通过主动sleep模拟这种情况）
+                TimeUnit.SECONDS.sleep(1);
                 log.info("kafka消息消费成功:topic:{} msg:{}",topic,msg);
             }catch (Exception e){
                 e.printStackTrace();
@@ -57,4 +68,5 @@ public class ProducerController {
             }
         }
     }
+
 }
---
+++ mq-demo/svr-mq/springboot-kafka-producer/src/main/java/com/noob/base/mq/service/FlowService.java
@@ -0,0 +1,10 @@
+package com.noob.base.mq.service;
+
+/**
+ * 模拟下游服务
+ */
+public interface FlowService {
+
+    public void handler();
+
+}
