5f8908877bfafc6854232ad38fb662e00f17f11b
holic-x <holic-x@foxmail.com>
2024-09-13 16:30:31 +0800

    docs：note updated
    docs：note updated

--- data-structure/algorithm-demo/src/com/noob/algorithm/leetcode/Solution128.java
+++ data-structure/algorithm-demo/src/com/noob/algorithm/leetcode/Solution128.java
@@ -33,7 +33,10 @@ public class Solution128 {
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5,6,7,8,9};
+//        int[] nums = {1,2,3,4,5,6,7,8,9};
+
+        // [9,1,4,7,3,-1,0,5,8,-1,6]
+        int[] nums = {9,1,4,7,3,-1,0,5,8,-1,6};
         System.out.println(new Solution128().longestConsecutive(nums));
     }
 
