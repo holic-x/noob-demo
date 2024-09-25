ca3df73735051815389adbf518aedf42701a3b92
holic-x <holic-x@foxmail.com>
2024-09-25 21:29:23 +0800

    fix：螺旋矩阵
    fix：螺旋矩阵

--- data-structure/algorithm-demo/src/com/noob/algorithm/leetcode/q54/Solution.java
+++ data-structure/algorithm-demo/src/com/noob/algorithm/leetcode/q54/Solution.java
@@ -20,20 +20,20 @@ public class Solution {
         int left = 0, right = m - 1, top = 0, bottom = n - 1;
 
         // 循环遍历4个方向，当指针相遇时候遍历停止
-        while(left <= right && top <= bottom) {
-            // 从左往右循环一遍，遍历完成top边界下移
+        while(left <= right && top <= bottom) { // 或者是list.size < m * n(因为res.size()==m×n`表示已经将当前矩阵所有元素塞入结果集了)
+            // 从左往右循环一遍（行不变），遍历完成top边界下移
             for(int i=left;i<=right;i++){
                 list.add(matrix[top][i]);
             }
             top++;// top边界下移
 
-            // 从上往下循环一遍，遍历完成right边界左移
+            // 从上往下循环一遍（列不变），遍历完成right边界左移
             for(int i=top;i<=bottom;i++){
                 list.add(matrix[i][right]);
             }
             right--;
 
-            // 从右往左循环一遍，遍历完成左边界右移
+            // 从右往左循环一遍（行不变），遍历完成左边界右移
             if(top<=bottom){
                 for(int i=right;i>=left;i--){
                     list.add(matrix[bottom][i]);
@@ -41,7 +41,7 @@ public class Solution {
             }
             bottom--;
 
-            // 从下往上循环一遍，遍历完成左边界右移
+            // 从下往上循环一遍（行不变），遍历完成左边界右移
             if(left<=right){
                 for(int i=bottom;i>=top;i--){
                     list.add(matrix[i][left]);
