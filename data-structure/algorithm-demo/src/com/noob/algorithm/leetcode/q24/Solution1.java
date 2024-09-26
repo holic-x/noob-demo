a470a501545b4bfa015e825ee88632c65e98d4f4
holic-x <holic-x@foxmail.com>
2024-09-26 18:55:19 +0800

    feat：两两交换链表中的节点
    feat：两两交换链表中的节点

---
+++ data-structure/algorithm-demo/src/com/noob/algorithm/leetcode/q24/Solution1.java
@@ -0,0 +1,36 @@
+package com.noob.algorithm.leetcode.q24;
+
+import java.util.HashSet;
+
+/**
+ * 24-两两交换链表中的节点
+ */
+public class Solution1 {
+
+    public ListNode swapPairs(ListNode head) {
+
+        
+
+    }
+
+}
+
+
+/**
+ * 链表节点定义
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
