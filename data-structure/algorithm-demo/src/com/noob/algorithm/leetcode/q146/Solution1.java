145ae24debf90def784d09555861df9bb9eaca70
holic-x <holic-x@foxmail.com>
2024-09-27 15:38:28 +0800

    fix：基于LinkedHashMap实现LRU缓存
    fix：基于LinkedHashMap实现LRU缓存

--- data-structure/algorithm-demo/src/com/noob/algorithm/leetcode/q146/Solution1.java
+++ data-structure/algorithm-demo/src/com/noob/algorithm/leetcode/q146/Solution1.java
@@ -12,24 +12,24 @@ class LRUCache {
     int capacity ;
 
     // 定义数据结构（存储集合）
-    HashMap <Integer, Integer> map;
+    LinkedHashMap <Integer, Integer> cache;
 
     // 构造函数
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        this.map = new LinkedHashMap<Integer, Integer>();
+        this.cache = new LinkedHashMap<Integer, Integer>();
     }
 
     // 访问元素（判断元素是否存在，更新访问节点位置（重新插入：即先删后插））
     public int get(int key) {
         // 如果指定值存在
-        if(map.containsKey(key)) {
+        if(cache.containsKey(key)) {
             // 记录当前值
-            int value = map.get(key);
+            int value = cache.get(key);
             // 移除节点
-            map.remove(key);
-            // 重新插入节点到队尾
-            map.put(key, value);
+            cache.remove(key);
+            // 重新插入节点到链表尾部
+            cache.put(key, value);
             return value;
         }else{
             return -1;
@@ -38,19 +38,20 @@ class LRUCache {
 
     // 插入元素（判断插入数据是否存在，存储则直接覆盖，如果不存在则继续判断是否超出阈值，超出阈值则需要触发LRU淘汰机制）
     public void put(int key, int value) {
-        if(map.containsKey(key)) {
-            // key已存在则覆盖
-            map.put(key, value);
+        if(cache.containsKey(key)) {
+            // key已存在则覆盖（先删后加）
+            cache.remove(key);
+            cache.put(key, value);
         }else{
             // 校验阈值
-            if(map.size()>=capacity){
+            if(cache.size()>=capacity){
                 // 超出阈值触发清理(因为LinkedHashMap是有序的，所以此处通过迭代器获取到第一个元素，然后直接移除)
-                Iterator<Integer> iterator = map.keySet().iterator();
-                map.remove(iterator.next());
+                Iterator<Integer> iterator = cache.keySet().iterator();
+                cache.remove(iterator.next());
             }
+            // 最终将新的数据节点插入
+            cache.put(key, value);
         }
-        // 最终将新的数据节点插入
-        map.put(key, value);
     }
 }
 
