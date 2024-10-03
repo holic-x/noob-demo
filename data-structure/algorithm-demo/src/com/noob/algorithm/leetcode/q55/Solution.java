a9689bf7719b71eab3a8a082c7d15cca96e4ffb1
holic-x <holic-x@foxmail.com>
2024-10-03 21:19:44 +0800

    docs：note updated
    docs：note updated

---
+++ data-structure/algorithm-demo/src/com/noob/algorithm/leetcode/q55/Solution.java
@@ -0,0 +1,13 @@
+package com.noob.algorithm.leetcode.q55;
+
+/**
+ * 55.跳跃游戏
+ */
+public class Solution {
+    public boolean canJump(int[] nums) {
+        // 倒序遍历
+        int end = nums.length - 1;
+        // 定义step记录
+        for(int i)
+    }
+}
