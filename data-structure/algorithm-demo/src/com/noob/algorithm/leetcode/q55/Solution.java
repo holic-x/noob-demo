9b84f49a1924bf874884e58d56d809beb9b53055
holic-x <holic-x@foxmail.com>
2024-10-03 22:08:30 +0800

    feat：算法 跳跃游戏（贪心算法）
    feat：算法 跳跃游戏（贪心算法）

--- data-structure/algorithm-demo/src/com/noob/algorithm/leetcode/q55/Solution.java
+++ data-structure/algorithm-demo/src/com/noob/algorithm/leetcode/q55/Solution.java
@@ -5,9 +5,15 @@ package com.noob.algorithm.leetcode.q55;
  */
 public class Solution {
     public boolean canJump(int[] nums) {
-        // 倒序遍历
-        int end = nums.length - 1;
-        // 定义step记录
-        for(int i)
+        int max = 0; // 此前可支撑的最大里程
+        for(int i=0;i<nums.length;i++){
+            // 判断当前的最大里程是否可以支持走到当前节点
+            if(max<i){
+                return false; // 无法走到当前节点
+            }
+            // 更新最大的覆盖范围（此处并不是简单的max+=nums[i]）
+            max = Math.max(max, i + nums[i]);
+        }
+        return true;
     }
 }
