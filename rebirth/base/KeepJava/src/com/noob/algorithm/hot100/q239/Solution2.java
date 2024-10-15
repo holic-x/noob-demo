f158860604ec9b2281e119bcb63c178ab76574b1
holic-x <holic-x@foxmail.com>
2024-10-15 23:02:31 +0800

    feat：239 滑动窗口最大值
    feat：239 滑动窗口最大值

---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/hot100/q239/Solution2.java
@@ -0,0 +1,19 @@
+package com.noob.algorithm.hot100.q239;
+
+/**
+ * 239 滑动窗口最大值
+ * 给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+ * 返回 滑动窗口中的最大值
+ */
+public class Solution2 {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        // 定义滑动窗口最大值
+        int[] max = new int[nums.length - k + 1];
+
+        // todo
+
+        return max;
+    }
+
+}
