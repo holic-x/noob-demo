8f078649c417269620a217e6fca96eea88766689
holic-x <holic-x@foxmail.com>
2024-10-16 17:18:18 +0800

    feat：反转链表
    feat：反转链表

--- rebirth/base/KeepJava/src/com/noob/algorithm/hot100/q160/ListNode.java
+++ rebirth/base/KeepJava/src/com/noob/algorithm/hot100/q160/ListNode.java
@@ -4,9 +4,9 @@ package com.noob.algorithm.hot100.q160;
  * 链表节点定义
  */
 public class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {
         val = x;
         next = null;
     }
