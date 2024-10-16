4eb807949974880e291422c6c2b44a845fdc2e0a
holic-x <holic-x@foxmail.com>
2024-10-16 16:58:32 +0800

    feat：相交链表
    feat：相交链表

---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/hot100/q160/ListNode.java
@@ -0,0 +1,13 @@
+package com.noob.algorithm.hot100.q160;
+
+/**
+ * 链表节点定义
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/hot100/q160/Solution.java
@@ -0,0 +1,40 @@
+package com.noob.algorithm.hot100.q160;
+
+/**
+ * 160 相交链表
+ */
+public class Solution {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        /**
+         * 定义两个指针：用于找公共链表节点
+         */
+
+        ListNode pointA = headA;
+        ListNode pointB = headB;
+
+        // 指针相遇则循环结束
+        while (pointA != pointB) {
+            // 遍历链表A，如果遍历到链表A尾部，则将其指向链表B
+            if(pointA != null) {
+                pointA = pointA.next;
+            }else{
+                pointA = headB;
+            }
+
+            // 遍历链表B，如果遍历到链表B尾部，则将其指向链表A
+            if(pointB != null){
+                pointB = pointB.next;
+            }else{
+                pointB = headA;
+            }
+        }
+        // 此时指针指向的就是公共交点，返回pointA、pointB都可以
+        return pointA;
+    }
+
+}
+
+
+
