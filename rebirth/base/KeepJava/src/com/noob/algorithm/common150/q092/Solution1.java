98707c533b0ea98d4dd5490ad7146b13b06606e4
holic-x <holic-x@foxmail.com>
2024-10-29 19:10:50 +0800

    feat：反转链表II
    feat：反转链表II

--- rebirth/base/KeepJava/src/com/noob/algorithm/common150/q092/Solution1.java
+++ rebirth/base/KeepJava/src/com/noob/algorithm/common150/q092/Solution1.java
@@ -18,27 +18,32 @@ public class Solution1 {
         ListNode dummy = new ListNode(-1, head); // 将head拼接过来构成一个新链表，对这个新链表进行区间反转操作：等价于ListNode dummy = new ListNode(-1); dummy.next = head;
         ListNode pd = dummy; // pd指针指向新链表的头节点
 
-        ListNode cur = head; // cur指针指向head链表的头节点
-
         /**
          * 第1段：正常遍历(p、cur向前移动，到达要进行反转的左区间)
-         * 遍历完成 cur 指向的节点就是是要翻转的节点
          * 遍历完成 pd 的下一个节点指向的是要翻转的节点（因为dummy多了个虚拟头结点）
          */
         for (int i = 0; i < left - 1; i++) {
             pd = pd.next;
-            cur = cur.next;
         }
 
         /**
          * 第2段：反转这个范围区间的链表节点
          */
-        for (int i = 0; i < right - left; i++) {
-            ListNode temp = cur.next ; // 保存当前需要翻转的节点后面的节点
-            
+        ListNode pre = null; // 指向前一个节点
+        ListNode cur = pd.next; // cur 指向当前要反转的节点（当前遍历的节点位置）
+        for (int i = 0; i < right - left + 1; i++) {
+            // 记录位置，翻转节点
+            ListNode nxt = cur.next; // 记录当前要翻转的下一个节点内容（避免被覆盖）
+            cur.next = pre; // 将cur.next指向前一个节点pre
+            // 更新指针位置，等待下一轮反转（pre、cur往后移动）
+            pre = cur;
+            cur = nxt;
         }
+        // 反转完成最终cur指向的就是反转区间的下一个节点（此处也就是指代第三段区间），而pre则是指向反转完成的这个区间
 
-
+        // 拼接：找到第一段的拼接位置，将反转后的区间和第三段区间进行拼接（此处是先操作pd.next.next,再操作pd.next，避免影响覆盖）
+        pd.next.next = cur;
+        pd.next = pre;
+        return dummy.next;
     }
-
 }
---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/common150/q206/Solution1.java
@@ -0,0 +1,27 @@
+package com.noob.algorithm.common150.q206;
+
+import com.noob.algorithm.common150.base.ListNode;
+
+/**
+ * 反转链表：遍历反转
+ */
+public class Solution1 {
+    public ListNode reverseList(ListNode head) {
+
+        ListNode cur = head; // 定义遍历指针
+
+        // 遍历并进行链表反转
+        ListNode pre = null; // 初始化
+        while(cur!=null){
+            // 反转链表
+            ListNode nxt = cur.next; // 记录链表的下一个节点
+            cur.next = pre;
+            // 指针往后移动等待下一轮反转
+            pre = cur;
+            cur = nxt;
+        }
+
+        // 返回结果
+        return pre;
+    }
+}
