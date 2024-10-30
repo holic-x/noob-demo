46587d6c151d4e5d8642b260e7407929fae5e818
holic-x <holic-x@foxmail.com>
2024-10-30 22:40:03 +0800

    feat：101 对称二叉树
    feat：101 对称二叉树

---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/common150/q101/Solution1.java
@@ -0,0 +1,34 @@
+package com.noob.algorithm.common150.q101;
+
+import com.noob.algorithm.common150.base.TreeNode;
+
+/**
+ * 101 对称二叉树
+ */
+public class Solution1 {
+
+    /**
+     * 转换题意：迭代思路
+     * 判断左右子树是否对称
+     */
+    public boolean isSymmetric(TreeNode root) {
+        return check(root.left, root.right);
+    }
+
+    /**
+     * 递归判断两个树是否对称
+     */
+    public boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+    }
+
+}
