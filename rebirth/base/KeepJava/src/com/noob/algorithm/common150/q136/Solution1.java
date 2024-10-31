888f2d54b0742fd75c841d158f4cbe9ae202cea7
holic-x <holic-x@foxmail.com>
2024-10-31 17:20:11 +0800

    feat： 136 只出现1次的数字
    feat： 136 只出现1次的数字

---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/common150/q136/Solution1.java
@@ -0,0 +1,22 @@
+package com.noob.algorithm.common150.q136;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 136 只出现1次的数字
+ */
+public class Solution1 {
+    public int singleNumber(int[] nums) {
+        Set<Integer> set = new HashSet();
+        for(int i = 0;i<nums.length;i++){
+            int cur = nums[i];
+            if(set.contains(cur)){
+                set.remove(cur);
+            }else{
+                set.add(cur);
+            }
+        }
+        return set.iterator().next();
+    }
+}
---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/common150/q136/Solution2.java
@@ -0,0 +1,23 @@
+package com.noob.algorithm.common150.q136;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 136 只出现1次的数字
+ */
+public class Solution2 {
+    public int singleNumber(int[] nums) {
+        /**
+         * 异或思路：
+         * 1. 0 和 任何数异或都等于它本身
+         * 2. 任何数 和 其本身异或等于0
+         * 3.满足交换律
+         */
+        int res = 0;
+        for(int num : nums){
+            res ^= num;
+        }
+        return res;
+    }
+}
