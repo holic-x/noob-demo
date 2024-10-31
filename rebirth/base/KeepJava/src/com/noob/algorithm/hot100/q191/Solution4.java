07cc2a37309d5a854d204770731d7912647a0e17
holic-x <holic-x@foxmail.com>
2024-10-31 17:01:10 +0800

    feat：191 位1的个数
    feat：191 位1的个数

--- rebirth/base/KeepJava/src/com/noob/algorithm/hot100/q191/Solution4.java
+++ rebirth/base/KeepJava/src/com/noob/algorithm/hot100/q191/Solution4.java
@@ -7,9 +7,13 @@ public class Solution4 {
 
     // 位运算
     public int hammingWeight(int n) {
-
-        
-
+        // 通过 n&(n-1) 移出最右边的1
+        int res = 0;
+        while (n != 0) {
+            n = n & (n - 1); // n 不为0，每次移出最右边的1，然后进行统计
+            res++;
+        }
+        return res;
     }
 
 }
