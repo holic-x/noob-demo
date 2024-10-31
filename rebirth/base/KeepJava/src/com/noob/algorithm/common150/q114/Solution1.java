84003981f56c0a1cc5d63dacc82076d8c226b4dd
holic-x <holic-x@foxmail.com>
2024-10-31 23:14:03 +0800

    feat：53 最大子序和
    feat：53 最大子序和

---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/common150/q114/Solution1.java
@@ -0,0 +1,17 @@
+package com.noob.algorithm.common150.q114;
+
+import com.noob.algorithm.common150.base.TreeNode;
+
+/**
+ * 114 二叉树展开为链表
+ */
+public class Solution1 {
+
+    /**
+     * 遍历的思路：
+     * 1.判断当前遍历节点是否存在左节点
+     */
+    public void flatten(TreeNode root) {
+
+    }
+}
