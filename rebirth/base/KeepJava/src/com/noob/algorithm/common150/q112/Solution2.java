b5da29d591003e5b5ee54db4f1dc98f3a7bce7c2
holic-x <holic-x@foxmail.com>
2024-11-04 10:42:20 +0800

    feat：112 路径之和
    feat：112 路径之和

---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/common150/q112/Solution2.java
@@ -0,0 +1,27 @@
+package com.noob.algorithm.common150.q112;
+
+import com.noob.algorithm.common150.base.TreeNode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 112 路径之和
+ */
+public class Solution2 {
+    /**
+     * 递归
+     */
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        // 递归出口
+        if (root == null) {
+            return false;
+        }
+        // 叶子节点判断
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+        // 递归判断
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
