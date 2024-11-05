f11b219b1f230c6c6576a542c80b2704dde96131
holic-x <holic-x@foxmail.com>
2024-11-05 08:23:35 +0800

    feat： 098 验证二叉搜索树
    feat： 098 验证二叉搜索树

---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/common150/q098/Solution4.java
@@ -0,0 +1,31 @@
+package com.noob.algorithm.common150.q098;
+
+import com.noob.algorithm.common150.base.TreeNode;
+
+/**
+ * 098 验证二叉搜索树
+ */
+public class Solution4 {
+
+    /**
+     * 递归方式：后序遍历
+     */
+    public boolean isValidBST(TreeNode root) {
+        return dfs(root)[1] != Long.MAX_VALUE;
+    }
+
+    private long[] dfs(TreeNode node) {
+        if (node == null) {
+            return new long[]{Long.MAX_VALUE, Long.MIN_VALUE};
+        }
+        long[] left = dfs(node.left);
+        long[] right = dfs(node.right);
+        long x = node.val;
+        // 也可以在递归完左子树之后立刻判断，如果发现不是二叉搜索树，就不用递归右子树了
+        if (x <= left[1] || x >= right[0]) {
+            return new long[]{Long.MIN_VALUE, Long.MAX_VALUE};
+        }
+        return new long[]{Math.min(left[0], x), Math.max(right[1], x)};
+    }
+
+}
