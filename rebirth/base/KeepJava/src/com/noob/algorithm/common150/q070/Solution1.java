8881d08e2085482bd9bd7c72fe754db80956f346
holic-x <holic-x@foxmail.com>
2024-11-07 11:53:40 +0800

    feat：爬楼梯 070
    feat：爬楼梯 070

---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/common150/q070/Solution1.java
@@ -0,0 +1,21 @@
+package com.noob.algorithm.common150.q070;
+
+/**
+ * 070 爬楼梯
+ */
+public class Solution1 {
+
+    public int climbStairs(int n) {
+        // 1.dp 数组定义(dp[i]表示爬i阶的方案个数)
+        int[] dp = new int[n+1];
+        // 2.初始化dp
+        dp[0] = 1; // 爬0阶有1种方案
+        dp[1] = 1; // 爬1阶有1种方案
+        // 3.构建dp
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        // 4.返回所求答案
+        return dp[n];
+    }
+}
