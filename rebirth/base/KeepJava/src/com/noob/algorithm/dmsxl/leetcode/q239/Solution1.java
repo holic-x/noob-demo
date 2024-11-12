e9a23ad96e38ad638bbb2c4a519d25d1fa7b94d5
holic-x <holic-x@foxmail.com>
2024-11-12 18:58:42 +0800

    feat：06-滑动窗口最大值（239）
    feat：06-滑动窗口最大值（239）

---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/dmsxl/leetcode/q239/Solution1.java
@@ -0,0 +1,23 @@
+package com.noob.algorithm.dmsxl.leetcode.q239;
+
+/**
+ * 239 滑动窗口的最大值
+ */
+public class Solution1 {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int len = nums.length;
+        // 1.定义数组存储滑动窗口的最大值
+        int[] maxArr = new int[len - k + 1]; // maxArr[i] 表示以i为起点的滑动窗口的最大值
+        // 2.暴力循环（确定窗口起点和终点，获取滑动窗口的最大值）
+        for (int i = 0; i < len - k + 1; i++) { // i 的临界取值受限于滑动窗口大小
+            // 统计区间的最大值
+            int curMax = Integer.MIN_VALUE;
+            for (int left = i; left < i + k; left++) {
+                curMax = Math.max(curMax, nums[left]); // 更新最大值
+            }
+            maxArr[i] = curMax; // 填充数组
+        }
+        // 返回结果集
+        return maxArr;
+    }
+}
