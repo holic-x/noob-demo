0e25c6aac4321c37d6d0e3711580a62d6d5f6944
holic-x <holic-x@foxmail.com>
2024-11-13 18:44:34 +0800

    feat：二叉树的遍历（广度优先、深度优先）
    feat：二叉树的遍历（广度优先、深度优先）

---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/dmsxl/leetcode/q144/Solution1.java
@@ -0,0 +1,30 @@
+package com.noob.algorithm.dmsxl.leetcode.q144;
+
+import com.noob.algorithm.dmsxl.baseStructure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 144 二叉树的前序遍历
+ */
+public class Solution1 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        // 初始化递归结果集
+        List<Integer> res = new ArrayList<>();
+        preorder(root,res);
+        return res;
+    }
+
+    // 递归方式实现前序遍历
+    public void preorder(TreeNode node,List<Integer> list){
+        // 递归出口
+        if(node==null){
+            return ;
+        }
+        // 递归过程（前序遍历DRL）
+        list.add(node.val);
+        preorder(node.left,list);
+        preorder(node.right,list);
+    }
+}
