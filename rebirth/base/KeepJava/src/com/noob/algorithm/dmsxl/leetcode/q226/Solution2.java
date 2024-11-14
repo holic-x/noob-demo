8338a307bd6cc6cdb9456c5188289fdfedb971fd
holic-x <holic-x@foxmail.com>
2024-11-14 14:30:25 +0800

    feat：翻转二叉树（226）
    feat：翻转二叉树（226）

---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/dmsxl/leetcode/q226/Solution2.java
@@ -0,0 +1,33 @@
+package com.noob.algorithm.dmsxl.leetcode.q226;
+
+import com.noob.algorithm.dmsxl.baseStructure.TreeNode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 226 翻转二叉树
+ */
+public class Solution2 {
+    // 思路：递归法
+    public TreeNode invertTree(TreeNode root) {
+        invertNode(root);
+        return root;
+    }
+
+    public void invertNode(TreeNode node){
+        // 递归出口
+        if (node == null) {
+            return ;
+        }
+        // 递归过程
+        // 交换左右节点
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+
+        // 继续递归交换左右子节点
+        invertNode(node.left);
+        invertNode(node.right);
+    }
+}
