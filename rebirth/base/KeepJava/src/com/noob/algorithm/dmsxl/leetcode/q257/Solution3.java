288ee2db858fca552e327bb0c67c422a8fc10858
holic-x <holic-x@foxmail.com>
2024-11-14 21:47:04 +0800

    feat：二叉树的所有路径（257）
    feat：二叉树的所有路径（257）

--- rebirth/base/KeepJava/src/com/noob/algorithm/dmsxl/leetcode/q257/Solution3.java
+++ rebirth/base/KeepJava/src/com/noob/algorithm/dmsxl/leetcode/q257/Solution3.java
@@ -3,32 +3,33 @@ package com.noob.algorithm.dmsxl.leetcode.q257;
 import com.noob.algorithm.dmsxl.baseStructure.TreeNode;
 
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
  * 257 二叉树的所有路径
  */
-public class Solution1 {
+public class Solution3 {
 
     /**
      * DFS 思路：类似地，遍历节点的同时 同步 更新路径信息
      */
     public List<String> binaryTreePaths(TreeNode root) {
-        if (root == null) {
+        if(root==null){
             return new ArrayList<>();
         }
         // 定义结果集，记录所有路径
         List<String> res = new ArrayList<>();
         List<String> curPath = new ArrayList<>();
-        dfs(root, res, curPath);
+        dfs(root,res,curPath);
         return res;
     }
 
     // DFS (DLR)
-    public void dfs(TreeNode node, List<String> res, List<String> curPath) {
+    public void dfs(TreeNode node,List<String> res,List<String> curPath){
         // 递归出口
-        if (node == null) {
+        if(node==null){
             return;
         }
 
@@ -36,18 +37,21 @@ public class Solution1 {
         curPath.add(String.valueOf(node.val));
 
         // 如果左、右子节点都为null，说明是叶子节点，同步更新路径信息
-        if (node.left == null && node.right == null) {
-            res.add(String.join("->", curPath));
-        } else {
-            // 如果子节点不为null，继续递归遍历子节点(需分别调用两次dfs，因此此处要处理回溯)
-            if (node.left != null) {
-                dfs(node.left, res, curPath);
-                curPath.remove(curPath.size() - 1); // 回溯：移除刚加入的最后一个元素
-            }
-            if (node.right != null) {
-                dfs(node.right, res, curPath);
-                curPath.remove(curPath.size() - 1); // 回溯：移除刚加入的最后一个元素
-            }
+        if(node.left==null && node.right==null){
+            res.add(String.join("->",curPath));
+        }else{
+            // 如果子节点不为null，继续递归遍历子节点
+            dfs(node.left,res,new ArrayList<>(curPath)); // 此处递归传入new一个对象，避免引用同一个对象导致问题
+            dfs(node.right,res,new ArrayList<>(curPath));
         }
     }
 }
+
+
+
+
+
+
+
+
+
--- rebirth/base/KeepJava/src/com/noob/algorithm/dmsxl/leetcode/q257/Solution4.java
+++ rebirth/base/KeepJava/src/com/noob/algorithm/dmsxl/leetcode/q257/Solution4.java
@@ -3,13 +3,12 @@ package com.noob.algorithm.dmsxl.leetcode.q257;
 import com.noob.algorithm.dmsxl.baseStructure.TreeNode;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
  * 257 二叉树的所有路径
  */
-public class Solution1 {
+public class Solution4 {
 
     /**
      * DFS 思路：类似地，遍历节点的同时 同步 更新路径信息
@@ -20,34 +19,28 @@ public class Solution1 {
         }
         // 定义结果集，记录所有路径
         List<String> res = new ArrayList<>();
-        List<String> curPath = new ArrayList<>();
-        dfs(root, res, curPath);
+        dfs(root, res, "");
         return res;
     }
 
     // DFS (DLR)
-    public void dfs(TreeNode node, List<String> res, List<String> curPath) {
+    public void dfs(TreeNode node, List<String> res, String curPath) {
         // 递归出口
         if (node == null) {
             return;
         }
 
         // node 不为null，记录路径信息并继续递归遍历
-        curPath.add(String.valueOf(node.val));
+        curPath = curPath + node.val;
 
         // 如果左、右子节点都为null，说明是叶子节点，同步更新路径信息
         if (node.left == null && node.right == null) {
             res.add(String.join("->", curPath));
         } else {
-            // 如果子节点不为null，继续递归遍历子节点(需分别调用两次dfs，因此此处要处理回溯)
-            if (node.left != null) {
-                dfs(node.left, res, curPath);
-                curPath.remove(curPath.size() - 1); // 回溯：移除刚加入的最后一个元素
-            }
-            if (node.right != null) {
-                dfs(node.right, res, curPath);
-                curPath.remove(curPath.size() - 1); // 回溯：移除刚加入的最后一个元素
-            }
+            curPath = curPath + "->"; // 分隔符
+            // 如果子节点不为null，继续递归遍历子节点
+            dfs(node.left, res, curPath);
+            dfs(node.right, res, curPath);
         }
     }
 }
