cd70ab0ebc49b52858ee65205f39a71548e246ab
holic-x <holic-x@foxmail.com>
2024-12-10 13:48:32 +0800

    feat：拓扑排序代码模板
    feat：拓扑排序代码模板

--- rebirth/base/KeepJava/src/com/noob/algorithm/dmsxl/graph/topologicalSort/TopologicalSortTemplate.java
+++ rebirth/base/KeepJava/src/com/noob/algorithm/dmsxl/graph/topologicalSort/TopologicalSortTemplate.java
@@ -1,4 +1,4 @@
-package com.noob.algorithm.dmsxl.kmw.q117;
+package com.noob.algorithm.dmsxl.graph.topologicalSort;
 
 
 import com.noob.algorithm.dmsxl.graph.Edge;
@@ -6,22 +6,29 @@ import com.noob.algorithm.dmsxl.graph.Edge;
 import java.util.*;
 
 /**
- * KMW 117 软件构建
+ * 拓扑排序代码模板
  */
-public class Solution1 {
+public class TopologicalSortTemplate {
 
     // N 个文件([0,N-1])、M 条边（有向依赖关系）
     public void topologicalSort(int n, List<Edge> edges) {
         // 1.记录每个节点的入度、以及当前节点指向什么节点
+
+        /**
+         * 定义节点和其关联节点列表（指向的节点列表）：
+         * 也可用Map<Integer,List<Integer>>集合形式构建映射关系
+         * 此处用List<List<Integer>>(下标表示节点，元素值表示节点指向的节点列表)
+         */
         List<List<Integer>> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) { // 初始化list
             list.add(new ArrayList<>());
         }
 
-        int[] inDegree = new int[n]; // 构建每个节点及其入度的依赖关系（基于数组构建）
+        // 构建每个节点及其入度的依赖关系（基于数组构建）
+        int[] inDegree = new int[n];
         for (Edge edge : edges) {
             int u = edge.u, v = edge.v; // u->v 关系校验
-            list.get(u).add(v); // 表示获取到u节点的指向节点列表然后追加相应的v节点（即处理u->v）
+            list.get(u).add(v); // 表示获取到u节点关联指向节点列表，然后追加相应的v节点（即处理u->v）
             inDegree[v]++; // u->v,入度是针对v节点校验
         }
 
@@ -33,13 +40,13 @@ public class Solution1 {
             }
         }
 
-        // 3.遍历处理
-        List<Integer> res = new ArrayList<>();
+        // 3.遍历处理(拓扑排序核心流程)：选出入度为0的节点进行处理（将其加入结果集、移出图）
+        List<Integer> res = new ArrayList<>(); // int[] res = new int[n]; int idx = 0; // 结果集遍历指针
         while (!queue.isEmpty()) {
             // 按顺序取出[入度为0]的节点进行处理
             int cur = queue.poll();
             // 将该节点加入结果集
-            res.add(cur);
+            res.add(cur); // res[idx++] = cur;
             // 将节点从图中移除（此处移除不是真的移除，而是处理边关系）
             List<Integer> link = list.get(cur);
             for (int node : link) {
@@ -67,20 +74,22 @@ public class Solution1 {
     public static void main(String[] args) {
         // 1.输入处理
         // N 个文件([0,N-1])、M 条边（有向依赖关系）
+        System.out.println("1.输入N个节点、M条边");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        // 接收文件依赖关系（实际对照的就是有向图的边关系）
-        List<Edge> links = new ArrayList<>();
+        System.out.println("2.输入M条边（a,b）形式");
+        // 接收边关系
+        List<Edge> edges = new ArrayList<>();
         while (m-- > 0) {
             int u = sc.nextInt();
             int v = sc.nextInt();
-            links.add(new Edge(u, v));
+            edges.add(new Edge(u, v));
         }
 
         // 2.调用拓扑排序
-        Solution1 solution = new Solution1();
-        solution.topologicalSort(n, links);
+        TopologicalSortTemplate solution = new TopologicalSortTemplate();
+        solution.topologicalSort(n, edges);
     }
 
 }
