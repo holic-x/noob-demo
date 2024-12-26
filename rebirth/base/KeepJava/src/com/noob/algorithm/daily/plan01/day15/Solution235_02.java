fb4e348d2d593b27b5cc3a5a7f658aa54cc94b80
holic-x <holic-x@foxmail.com>
2024-12-26 09:39:59 +0800

    feat：235 二叉搜索树的最近公共祖先
    feat：235 二叉搜索树的最近公共祖先

---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan01/day15/Solution235_02.java
@@ -0,0 +1,43 @@
+package com.noob.algorithm.daily.plan01.day15;
+
+import com.noob.algorithm.daily.base.TreeNode;
+
+/**
+ * 🟡 235 二叉搜索树的最近公共祖先
+ */
+public class Solution235_02 {
+
+    public TreeNode lowestCommonAncestor(TreeNode node, TreeNode p, TreeNode q) {
+        return dfs(node, p, q);
+    }
+
+    /**
+     * 穷举法思路：（二叉树的最近公共祖先解法）
+     * 1.node==null || node==p || node==q : 返回节点return node
+     * 2.node!=null 进一步递归判断是在左子树还是右子树
+     * - 如果左右子树都有则说明是当前节点：return node
+     * - 如果在左子树或者右子树，则返回不为null的那个子树节点
+     */
+    public TreeNode dfs(TreeNode node, TreeNode p, TreeNode q) {
+        if (node == null || node == p || node == q) {
+            return node;
+        }
+
+        // 根据当前遍历节点值和p、q节点的值进行比较
+        if ((node.val >= p.val && node.val <= q.val) || node.val >= q.val && node.val <= p.val) {
+            return node;
+        }
+
+        // 均小于p\q
+        if (node.val < p.val && node.val < q.val) {
+            return dfs(node.right, p, q);
+        }
+
+        // 均大于p\q
+        if (node.val > p.val && node.val > q.val) {
+            return dfs(node.left, p, q);
+        }
+
+        return null;
+    }
+}
