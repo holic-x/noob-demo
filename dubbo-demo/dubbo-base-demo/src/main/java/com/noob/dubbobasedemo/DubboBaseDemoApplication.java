96b5dd567ae23bb6ae36437a0eb8bdc48e118074
holic-x <holic-x@foxmail.com>
2024-12-30 16:16:43 +0800

    feat：dubbo-base-demo dubbo 核心基础
    feat：dubbo-base-demo dubbo 核心基础

---
+++ dubbo-demo/dubbo-base-demo/src/main/java/com/noob/dubbobasedemo/DubboBaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.noob.dubbobasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboBaseDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboBaseDemoApplication.class, args);
+    }
+
+}
---
+++ dubbo-demo/dubbo-base-demo/src/test/java/com/noob/dubbobasedemo/DubboBaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noob.dubbobasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboBaseDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
