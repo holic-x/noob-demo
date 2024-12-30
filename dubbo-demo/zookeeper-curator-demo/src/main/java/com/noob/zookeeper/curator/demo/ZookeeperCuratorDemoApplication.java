bf3bbe63571cac32d21b459393df43806a99e110
holic-x <holic-x@foxmail.com>
2024-12-30 10:48:45 +0800

    feat：zookeeper curator demo
    feat：zookeeper curator demo

---
+++ dubbo-demo/zookeeper-curator-demo/src/main/java/com/noob/zookeeper/curator/demo/ZookeeperCuratorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.noob.zookeeper.curator.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookeeperCuratorDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZookeeperCuratorDemoApplication.class, args);
+    }
+
+}
