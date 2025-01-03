cde4b872c00895e2a4f6008b5fb783774b855356
holic-x <holic-x@foxmail.com>
2025-01-03 14:56:19 +0800

    refactor：代码结构调整

--- dubbo-demo/rpc-simple-demo/src/main/java/com/noob/rpc/simple/RpcSimpleDemoApplication.java
+++ dubbo-demo/rpc-simple-demo/src/main/java/com/noob/rpc/simple/RpcSimpleDemoApplication.java
@@ -1,4 +1,4 @@
-package com.noob.rpcsimpledemo;
+package com.noob.rpc.simple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- dubbo-demo/rpc-simple-demo/src/test/java/com/noob/rpc/simple/demo/RpcSimpleDemoApplicationTests.java
+++ dubbo-demo/rpc-simple-demo/src/test/java/com/noob/rpc/simple/demo/RpcSimpleDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.noob.rpcsimpledemo;
+package com.noob.rpc.simple.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
