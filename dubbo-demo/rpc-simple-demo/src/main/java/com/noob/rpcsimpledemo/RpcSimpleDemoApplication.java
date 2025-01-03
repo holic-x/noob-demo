3501852ee1823366ab27a5176480dd0d462d52e0
holic-x <holic-x@foxmail.com>
2025-01-03 11:14:39 +0800

    feat：rpc-simple-demo
    feat：rpc-simple-demo

---
+++ dubbo-demo/rpc-simple-demo/src/main/java/com/noob/rpcsimpledemo/RpcSimpleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.noob.rpcsimpledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcSimpleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpcSimpleDemoApplication.class, args);
+    }
+
+}
