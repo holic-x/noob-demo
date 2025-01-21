807b4c211d3a69b193383f9651c0c20be89bb4fd
holic-x <holic-x@foxmail.com>
2025-01-21 10:20:07 +0800

    feat：动态规划 打家劫舍系列题型
    feat：动态规划 打家劫舍系列题型

---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan01/day33/Solution337_02.java
@@ -0,0 +1,53 @@
+package com.noob.algorithm.daily.plan01.day33;
+
+import com.noob.algorithm.daily.base.TreeNode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 🟡 337 打家劫舍III - https://leetcode.cn/problems/house-robber-iii/description/
+ */
+public class Solution337_02 {
+
+    private Map<TreeNode, Integer> map = new HashMap<>();
+
+    /**
+     * 思路：基于深度优先遍历思路，遍历每个节点，确定偷窃方案
+     * ① 偷 当前节点：则不能偷其子节点，只能偷其子节点的子节点
+     * ② 不偷 当前节点：则考虑偷其子节点的方案
+     * 优化：记忆化推导（针对已经遍历过的节点则可以直接返回，而不需要重复计算）
+     */
+    public int rob(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        // 如果节点已经计算过则直接返回，不需要重复计算
+        if (map.containsKey(root)) {
+            return map.get(root);
+        }
+
+        // ① 偷当前节点
+        int robAmount1 = root.val;
+        if (root.left != null) {
+            robAmount1 += (rob(root.left.left) + rob(root.left.right));
+        }
+        if (root.right != null) {
+            robAmount1 += (rob(root.right.left) + rob(root.right.right));
+        }
+
+        // ② 不偷当前节点
+        int robAmount2 = rob(root.left) + rob(root.right);
+
+        int maxAmount = Math.max(robAmount1, robAmount2);
+
+        // 记录当前节点
+        map.put(root, maxAmount);
+
+        // 返回两种盗窃方案的最大值
+        return maxAmount;
+    }
+}
---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan01/day33/Solution337_03.java
@@ -0,0 +1,51 @@
+package com.noob.algorithm.daily.plan01.day33;
+
+import com.noob.algorithm.daily.base.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 🟡 337 打家劫舍III - https://leetcode.cn/problems/house-robber-iii/description/
+ */
+public class Solution337_03 {
+
+    /**
+     * 动态规划思路：对于每个节点都记录偷、不偷的情况下偷窃金额
+     * ① 偷 当前节点：则不能偷其子节点，只能偷其子节点的子节点
+     * ② 不偷 当前节点：则考虑偷其子节点的方案
+     */
+    public int rob(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int[] amount = dfs(root);
+        return Math.max(amount[0], amount[1]);
+    }
+
+    private int[] dfs(TreeNode node) {
+        // 递归出口
+        if (node == null) {
+            return new int[]{0, 0};
+        }
+        // 1.dp 定义：dp[0]表示不偷、dp[1]表示偷
+        int[] dp = new int[2];
+
+        /**
+         * 2.dp 递推：
+         * ① 不偷：选择不偷当前节点，判断其左右孩子的情况（左、右孩子各自也可能偷或者不偷）: max{偷左节点，不偷左节点} + max{偷右节点，不偷右节点}
+         * ② 偷：选择偷当前节点，则不能偷左右孩子：val(偷当前节点) + 不偷左节点 + 不偷右节点
+         */
+        int[] left = dfs(node.left); // 左节点的偷盗情况
+        int[] right = dfs(node.right); // 右节点的偷盗情况
+
+        // ① 不偷
+        int robAmount1 = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+
+        // ② 偷
+        int robAmount2 = node.val + left[0] + right[0];
+
+        // 返回偷窃方案
+        return new int[]{robAmount1, robAmount2};
+    }
+}
