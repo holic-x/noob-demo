6627f148dcd79df76bb8358d5c383df28648adc2
Huh-x <97139830+Huh-x@users.noreply.github.com>
2025-02-02 18:00:28 +0800

    feat：日常算法训练
    feat：日常算法训练

---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan01/day38/Solution072_01.java
@@ -0,0 +1,4 @@
+package com.noob.algorithm.daily.plan01.day38;
+
+public class Solution072_01 {
+}
---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan01/day38/Solution115_01.java
@@ -0,0 +1,5 @@
+package com.noob.algorithm.daily.plan01.day38;
+
+public class Solution115_01 {
+
+}
