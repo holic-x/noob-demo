abc25d12376b99b2c5eb05be633a982556bf1e7d
holic-x <holic-x@foxmail.com>
2025-02-05 13:59:14 +0800

    feat：739 每日温度
    feat：739 每日温度

---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan01/day40/Solution739_01.java
@@ -0,0 +1,30 @@
+package com.noob.algorithm.daily.plan01.day40;
+
+import java.util.Arrays;
+
+/**
+ * 🟡 739 每日温度 - https://leetcode.cn/problems/daily-temperatures/description/
+ */
+public class Solution739_01 {
+
+    /**
+     * 思路分析：模拟法
+     * 遍历每个温度，判断其后比它高的温度出现在哪个位置
+     */
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] t = new int[n];
+        Arrays.fill(t, 0); // 初始化为0
+        // 外层遍历定位每个温度，内层遍历寻找比当前温度大的下一个温度
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (temperatures[j] > temperatures[i]) {
+                    t[i] = j - i;
+                    break; // 寻找到下一个更高的温度，填充结果并跳出当次循环
+                }
+            }
+        }
+        // 返回结果
+        return t;
+    }
+}
