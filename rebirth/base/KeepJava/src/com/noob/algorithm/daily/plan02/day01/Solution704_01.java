33516fea9967f979e2ecf58e0d119573895f4423
holic-x <holic-x@foxmail.com>
2025-02-10 15:21:13 +0800

    feat：704 二分查找
    feat：704 二分查找

--- rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan02/day01/Solution704_01.java
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan02/day01/Solution704_01.java
@@ -10,7 +10,7 @@ public class Solution704_01 {
     }
 
     // 二分查找（闭区间）
-    private int binarySearch(int[] nums, int target) {
+    private int binarySearch1(int[] nums, int target) {
         int n = nums.length;
         // 定义双指针确定检索区间
         int left = 0, right = n - 1; // 闭区间
@@ -22,6 +22,50 @@ public class Solution704_01 {
             } else if (target < nums[mid]) {
                 // target 在左侧，右边界移动
                 right = mid - 1;
+            } else if (target > nums[mid]) {
+                // target 在右侧，左边界移动
+                left = mid + 1;
+            }
+        }
+        // 未找到目标值
+        return -1;
+    }
+
+    // 二分查找（开区间）
+    private int binarySearch2(int[] nums, int target) {
+        int n = nums.length;
+        // 定义双指针确定检索区间
+        int left = -1, right = n; // 开区间
+        while (left + 1 < right) { // 开区间控制
+            int mid = left + (right - left) / 2; // 避免数值溢出问题
+            // 校验target和nums[mid]的大小，确定范围缩减方向
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target < nums[mid]) {
+                // target 在左侧，右边界移动
+                right = mid;
+            } else if (target > nums[mid]) {
+                // target 在右侧，左边界移动
+                left = mid;
+            }
+        }
+        // 未找到目标值
+        return -1;
+    }
+
+    // 二分查找（左闭右开区间）
+    private int binarySearch(int[] nums, int target) {
+        int n = nums.length;
+        // 定义双指针确定检索区间
+        int left = 0, right = n; // 左闭右开区间[left,right)
+        while (left < right) { // 左闭右开区间[left,right)控制
+            int mid = left + (right - left) / 2; // 避免数值溢出问题
+            // 校验target和nums[mid]的大小，确定范围缩减方向
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target < nums[mid]) {
+                // target 在左侧，右边界移动
+                right = mid;
             } else if (target > nums[mid]) {
                 // target 在右侧，左边界移动
                 left = mid + 1;
