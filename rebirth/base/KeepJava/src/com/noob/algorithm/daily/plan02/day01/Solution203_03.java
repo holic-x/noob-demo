b2dd251b695950ab6ea51c4c97aeacd38913de7a
holic-x <holic-x@foxmail.com>
2025-02-10 17:31:53 +0800

    feat：203 移除链表元素
    feat：203 移除链表元素

---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan02/day01/Solution203_03.java
@@ -0,0 +1,23 @@
+
+
+package com.noob.algorithm.daily.plan02.day01;
+
+import com.noob.algorithm.daily.base.ListNode;
+
+/**
+ * 🟢 203 移除链表元素 - https://leetcode.cn/problems/remove-linked-list-elements/description/
+ */
+public class Solution203_03 {
+
+    /**
+     * 思路分析：递归法
+     * 删除链表元素（val）
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) {
+            return head;
+        }
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;
+    }
+}
