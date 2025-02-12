20e007e45b6198279c0df5f0701de64b04a63ad7
holic-x <holic-x@foxmail.com>
2025-02-12 11:34:47 +0800

    feat：二叉树的遍历（DFS、BFS）
    feat：二叉树的遍历（DFS、BFS）

---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan02/day03/p006/Solution094_02.java
@@ -0,0 +1,46 @@
+package com.noob.algorithm.daily.plan02.day03.p006;
+
+import com.noob.algorithm.common150.base.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 🟢 094 二叉树的中序遍历 -  https://leetcode.cn/problems/binary-tree-inorder-traversal/description/
+ */
+public class Solution094_02 {
+
+    /**
+     * 迭代思路：LDR
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        List<Integer> list = new ArrayList<>();
+        // 构建栈辅助遍历
+        Stack<TreeNode> stack = new Stack<>();
+        // stack.push(root);
+        // 定义遍历指针
+        TreeNode cur = root;
+        while (!stack.isEmpty() || cur != null) {
+            // 如果存在左节点则入栈
+            if (cur != null) {
+                // 将遍历节点依次入栈
+                stack.push(cur);
+                cur = cur.left; // 向左遍历
+            } else if (cur == null) {
+                // 到达了最左节点，则可取出栈顶元素处理，随后将cur指针指向右节点
+                TreeNode node = stack.pop();
+                list.add(node.val);
+                cur = node.right; // 将遍历指针指向当前取出节点的右节点
+            }
+        }
+
+        // 返回结果集
+        return list;
+    }
+
+}
---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan02/day03/p006/Solution102_01.java
@@ -0,0 +1,51 @@
+package com.noob.algorithm.daily.plan02.day03.p006;
+
+import com.noob.algorithm.common150.base.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 🟡 102 二叉树的层序遍历 - https://leetcode.cn/problems/binary-tree-level-order-traversal/
+ */
+public class Solution102_01 {
+
+    /**
+     * 层序遍历：从上到下、从左到右 分层遍历
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        // root 校验
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        // 定义结果集合
+        List<List<Integer>> res = new ArrayList<>();
+        // 构建队列辅助遍历
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root); // 初始化
+        // 队列不为空，继续遍历处理
+        while (!queue.isEmpty()) {
+            // 计算当层遍历元素个数
+            int curSize = queue.size();
+            List<Integer> temp = new ArrayList<>();
+            for (int i = 0; i < curSize; i++) {
+                // 取出队列元素
+                TreeNode node = queue.poll();
+                temp.add(node.val);
+                // 左右子节点处理
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            // 当层遍历结束，填充结果集
+            res.add(temp);
+        }
+        // 返回结果集合
+        return res;
+    }
+}
