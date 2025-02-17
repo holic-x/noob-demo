35cad62da7841c944f807835e6c212ed47efccfa
holic-x <holic-x@foxmail.com>
2025-02-17 15:18:39 +0800

    feat：贪心算法
    feat：贪心算法

---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan02/day07/p019/Solution455_01.java
@@ -0,0 +1,36 @@
+package com.noob.algorithm.daily.plan02.day07.p019;
+
+import java.util.Arrays;
+
+/**
+ * 🟢 455 分发饼干 - https://leetcode.cn/problems/assign-cookies/description/
+ */
+public class Solution455_01 {
+
+    /**
+     * 思路分析：g[]胃口值、s[]饼干尺寸，只有s[]满足(大于等于)g[]才可以满足胃口
+     */
+    public int findContentChildren(int[] g, int[] s) {
+        // 贪心思路：将g、s从小到大进行排序，小饼干给小胃口的孩子，大饼干给大胃口的孩子
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int gLen = g.length, sLen = s.length;
+        // 定义指针分别用于指向当前待分配小孩胃口、当前待分配饼干
+        int gPointer = 0, sPointer = 0;
+        int cnt = 0;
+        while (gPointer < gLen && sPointer < sLen) {
+            // 如果饼干满足分配条件，则直接分配
+            if (s[sPointer] >= g[gPointer]) {
+                cnt++;
+                // 指针移动
+                gPointer++;
+                sPointer++;
+            } else {
+                // 找下一块饼干（当前饼干不能满足目前最小胃口的小孩，则更不可能满足后面的孩子）
+                sPointer++;
+            }
+        }
+        // 返回分配结果
+        return cnt;
+    }
+}
---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan02/day07/p019/Solution860_01.java
@@ -0,0 +1,46 @@
+package com.noob.algorithm.daily.plan02.day07.p019;
+
+/**
+ * 🟢 860 柠檬水找零 - https://leetcode.cn/problems/lemonade-change/description/
+ */
+public class Solution860_01 {
+
+    /**
+     * 思路分析：顾客支付5 10 20，一杯柠檬水5元，判断是否可以正确找零
+     * 找零思路：基于贪心思路，找零的目的是为了保留更多的零钱来支撑找零操作，因此每次优先选择可以保留更多零钱的方案
+     */
+    public boolean lemonadeChange(int[] bills) {
+        // 定义目前零钱张数：此处只有5 10两种零钱可支持找零操作
+        int five = 0, ten = 0;
+        // 遍历账单方案进行处理
+        for (int num : bills) {
+            // 分5 10 20这三种账单情况讨论
+            if (num == 5) {
+                five++; // 无需找零，5元入账
+            } else if (num == 10) {
+                // 校验是否可以找零
+                if (five <= 0) {
+                    return false;
+                }
+                // 可以找零，则10元入账，5元出账
+                ten++;
+                five--;
+            } else if (num == 20) {
+                // 有两种找零方案（5+5+5、5+10），为了尽可能保留更多零钱，优先选择5+10方案
+                if (five > 0 && ten > 0) {
+                    // 20入账，10+5出账
+                    ten--;
+                    five--;
+                } else if (five >= 3) {
+                    // 20入账，5+5+5出账
+                    five = five - 3;
+                } else {
+                    return false;// 其余情况不满足找零条件
+                }
+            }
+        }
+        // 所有账单数据处理完成，说明目前可支持找零操作
+        return true;
+    }
+
+}
