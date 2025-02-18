c5ac0c4fead8705824ca24e37d9a3a5f3c05bbaa
holic-x <holic-x@foxmail.com>
2025-02-18 12:01:53 +0800

    feat：完全背包问题
    feat：完全背包问题

---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan02/day08/p027/Solution377_01.java
@@ -0,0 +1,47 @@
+package com.noob.algorithm.daily.plan02.day08.p027;
+
+import java.util.Arrays;
+
+/**
+ * 🟡 377 组合总和IV - https://leetcode.cn/problems/combination-sum-iv/description/
+ */
+public class Solution377_01 {
+
+    private int cnt = 0; // 满足条件的组合个数
+    private int curPathSum = 0; // 当前路径和
+
+    /**
+     * 思路分析：不同整数组成nums，求可以构成target的元素组合个数（元素可以重复利用）
+     * 回溯法
+     */
+    public int combinationSum4(int[] nums, int target) {
+        // 对数组元素进行排序
+        Arrays.sort(nums);
+        // 调用回溯算法
+        backTrack(0, nums, target);
+        // 返回结果
+        return cnt;
+    }
+
+    // 回溯算法
+    private void backTrack(int idx, int[] nums, int target) {
+        // 递归出口
+        if (curPathSum > target) {
+            return;
+        }
+        // 结果收集
+        if (curPathSum == target) {
+            cnt++;
+            return;
+        }
+        // 回溯处理
+        for (int i = idx; i < nums.length; i++) {
+            // 选择元素
+            curPathSum += nums[i];
+            // 递归处理（元素可以重复利用）
+            backTrack(idx, nums, target);
+            // 恢复现场
+            curPathSum -= nums[i];
+        }
+    }
+}
---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan02/day08/p027/Solution377_02.java
@@ -0,0 +1,41 @@
+package com.noob.algorithm.daily.plan02.day08.p027;
+
+import java.util.Arrays;
+
+/**
+ * 🟡 377 组合总和IV - https://leetcode.cn/problems/combination-sum-iv/description/
+ */
+public class Solution377_02 {
+
+
+    /**
+     * 思路分析：不同整数组成nums，求可以构成target的元素组合个数（元素可以重复利用）
+     * 动态规划
+     */
+    public int combinationSum4(int[] nums, int target) {
+        int m = nums.length, n = target + 1;
+        // 1.dp 定义：dp[j] 表示[0,i]的物品可以凑满 j 的元素组合个数
+        int[] dp = new int[n];
+
+        /**
+         * 2.dp 递推
+         * - j >= nums[i]: dp[j] = dp[j] + dp[j-nums[i]]
+         * - j < nums[i]: dp[j] = dp[j] 继承状态
+         */
+
+        // 3.dp 初始化
+        dp[0] = 1; // 凑满0的方案只有1种就是不凑
+
+        // 4.dp 构建：遍历顺序（排列概念：先背包后物品，背包正序）
+        for (int j = 1; j < n; j++) {
+            for (int i = 0; i < m; i++) {
+                dp[j] = (j >= nums[i] ? dp[j] + dp[j - nums[i]] : dp[j]);
+            }
+        }
+
+        // 返回结果
+        return dp[target];
+    }
+
+
+}
