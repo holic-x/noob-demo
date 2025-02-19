fed7f85e6f05ccb6198885c614a69271811c3528
holic-x <holic-x@foxmail.com>
2025-02-19 11:40:55 +0800

    feat：单调栈 init
    feat：单调栈 init

---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan02/day11/p033/Solution496_01.java
@@ -0,0 +1,12 @@
+package com.noob.algorithm.daily.plan02.day11.p033;
+
+/**
+ * 🟢 496 下一个更大元素 I - https://leetcode.cn/problems/next-greater-element-i/description/
+ */
+public class Solution496_01 {
+
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+        return null;
+    }
+}
---
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan02/day11/p033/Solution503_01.java
@@ -0,0 +1,10 @@
+package com.noob.algorithm.daily.plan02.day11.p033;
+
+/**
+ * 🟡 503 下一个更大元素II - https://leetcode.cn/problems/next-greater-element-ii/description/
+ */
+public class Solution503_01 {
+    public int[] nextGreaterElements(int[] nums) {
+        return null;
+    }
+}
