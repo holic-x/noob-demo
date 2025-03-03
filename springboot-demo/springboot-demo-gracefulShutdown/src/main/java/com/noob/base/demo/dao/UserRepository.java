0c733f1dd931699f444a558c00baeee158a956f5
holic-x <holic-x@foxmail.com>
2025-03-03 21:41:03 +0800

    fix：springboot 优雅停机测试
    fix：springboot 优雅停机测试

--- springboot-demo/springboot-demo-gracefulShutdown/src/main/java/com/noob/base/demo/dao/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.noob.base.demo.dao;
-
-import com.noob.base.demo.model.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
