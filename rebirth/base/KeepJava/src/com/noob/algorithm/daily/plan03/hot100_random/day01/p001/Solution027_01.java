fb25151536019250647aa411e568fd463ee3f8d8
holic-x <holic-x@foxmail.com>
2025-04-18 08:50:27 +0800

    feat：027 移除元素
    feat：027 移除元素

--- rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_random/day01/p001/Solution027_01.java
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_random/day01/p001/Solution027_01.java
@@ -5,9 +5,18 @@ package com.noob.algorithm.daily.plan03.hot100_random.day01.p001;
  */
 public class Solution027_01 {
     /**
-     * 思路分析：
+     * 思路分析：双指针思路，覆盖数据概念
      */
     public int removeElement(int[] nums, int val) {
-        return 0;
+        // 定义指针用于指向当前覆盖位置
+        int curIdx = 0;
+        // 遍历数组，校验当前遍历元素是否为val，如果为val则跳过，非val则将当前元素覆盖到curIdx指向位置
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[curIdx] = nums[i];
+                curIdx++; // 指针移动指向下一个位置
+            }
+        }
+        return curIdx;
     }
 }
