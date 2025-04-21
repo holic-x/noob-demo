c452980d75c6907804f198963227e21527e32e7a
holic-x <holic-x@foxmail.com>
2025-04-21 08:34:12 +0800

    feat：977 有序数组的平方
    feat：977 有序数组的平方

--- rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_random/day01/p001/Solution977_01.java
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_random/day01/p001/Solution977_01.java
@@ -7,8 +7,33 @@ public class Solution977_01 {
 
     /**
      * 思路分析：
+     * 双指针处理
      */
     public int[] sortedSquares(int[] nums) {
-        return nums;
+        int n = nums.length;
+        // 定义平方和的结果集，遍历顺序从左往右，填充顺序从右往左
+        int[] ans = new int[n];
+
+        // 定义数据填充指针位置
+        int idx = n - 1; // 从后往前填充数据
+        int left = 0, right = n - 1; // 双指针
+
+        while (left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+            // 判断当前遍历元素位置与当前填充位置指向的元素的平方和大小
+            if (leftSquare > rightSquare) {
+                ans[idx] = leftSquare;
+                idx--;
+                left++;
+            } else {
+                ans[idx] = rightSquare;
+                idx--;
+                right--;
+            }
+        }
+
+        // 返回结果
+        return ans;
     }
 }
