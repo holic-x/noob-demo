aff25e46d9e2c39dac267f76d3cffc19623b80e6
holic-x <holic-x@foxmail.com>
2025-04-24 08:48:51 +0800

    feat：日常算法训练
    feat：日常算法训练

--- rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_random/day01/p002/Solution206_01.java
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_random/day01/p002/Solution206_01.java
@@ -10,8 +10,21 @@ public class Solution206_01 {
 
     /**
      * 思路分析：
+     * 反转链表
      */
     public ListNode reverseList(ListNode head) {
-        return null;
+        ListNode pre = null;
+        ListNode cur = head;
+
+        while(cur!=null){
+            ListNode nxt = cur.next;
+            // 调整指针
+            cur.next = pre;
+            // 更新指针位置
+            pre = cur;
+            cur = nxt;
+        }
+
+        return pre;
     }
 }
--- rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_random/day01/p002/Solution707_01.java
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_random/day01/p002/Solution707_01.java
@@ -1,5 +1,7 @@
 package com.noob.algorithm.daily.plan03.hot100_random.day01.p002;
 
+import com.noob.algorithm.plan_archive.baseStructure.ListNode;
+
 /**
  * 🟡 707 设计链表 - https://leetcode.cn/problems/design-linked-list/description/
  */
@@ -16,3 +18,96 @@ public class Solution707_01 {
  * obj.addAtIndex(index,val);
  * obj.deleteAtIndex(index);
  */
+// 自定义单链表
+class MyLinkedList {
+
+    // 定义链表
+    ListNode head;
+
+    // 定义链表长度(有效的index集合，维护当前链表长度)
+    int size;
+
+    // 构造器：初始化链表
+    public MyLinkedList() {
+        head = new ListNode(-1);
+    }
+
+    // 获取指定索引位置的元素
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            return -1; // 非法索引
+        }
+
+        // 遍历
+        ListNode cur = head;
+        for (int i = 0; i <= index; i++) {
+            cur = cur.next;
+        }
+
+        return cur.val;
+    }
+
+    // addAtHead: 头插
+    public void addAtHead(int val) {
+        // 构建新节点
+        ListNode newNode = new ListNode(val);
+        // 头插
+        ListNode nxt = head.next;
+        head.next = newNode;
+        newNode.next = nxt;
+        // size 更新
+        size++;
+    }
+
+    // addAtTail: 尾插
+    public void addAtTail(int val) {
+        // 遍历到最后一个节点，随后执行尾插操作
+        ListNode cur = head;
+        for (int i = 0; i < size; i++) {
+            cur = cur.next;
+        }
+        // 插入
+        cur.next = new ListNode(val);
+        // size 更新
+        size++;
+    }
+
+    // addAtIndex: 指定位置插入
+    public void addAtIndex(int index, int val) {
+        // index 范围校验
+        if (index < 0 || index >= size) {
+            return;
+        }
+
+        // 遍历到指定位置，随后执行操作
+        ListNode cur = head;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        // 插入
+        ListNode newNode = new ListNode(val);
+        ListNode nxt = cur.next;
+        cur.next = newNode;
+        newNode.next = nxt;
+        // size 更新
+        size++;
+    }
+
+    // deleteAtIndex:删除指定索引位置元素
+    public void deleteAtIndex(int index) {
+        // index 范围校验
+        if (index < 0 || index >= size) {
+            return;
+        }
+
+        // 寻找待删除节点的前驱节点
+        ListNode cur = head;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        cur.next = cur.next.next;
+        // size 更新
+        size--;
+    }
+
+}
