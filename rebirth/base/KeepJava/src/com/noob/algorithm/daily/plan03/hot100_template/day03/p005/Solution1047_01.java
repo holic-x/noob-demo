a132389de48deb0d3fcdb4a024477d1d0f5a505c
holic-x <holic-x@foxmail.com>
2025-05-08 09:47:17 +0800

    feat：1047 删除字符串中的所有相邻重复项
    feat：1047 删除字符串中的所有相邻重复项

--- rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_template/day03/p005/Solution1047_01.java
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_template/day03/p005/Solution1047_01.java
@@ -8,9 +8,34 @@ import java.util.Stack;
 public class Solution1047_01 {
 
     /**
+     * 概要：删除两个相邻且相同的字母
      * 思路分析：
      */
     public String removeDuplicates(String s) {
-        return null;
+        // 构建栈辅助存储
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            // 校验栈顶元素与当前遍历元素
+            if (!stack.isEmpty()) {
+                char top = stack.peek();
+                if (top == ch) {
+                    // 弹出栈顶元素
+                    stack.pop();
+                } else {
+                    stack.push(ch);
+                }
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        // 弹出栈内留存元素并拼接为字符串
+        StringBuffer buffer = new StringBuffer();
+        while (!stack.isEmpty()) {
+            buffer.append(stack.pop());
+        }
+
+        // 返回结果
+        return buffer.reverse().toString();
     }
 }
