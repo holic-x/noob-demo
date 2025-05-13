7d1ea3e75929cdb826992fb2ce0c9167c62d4f72
holic-x <holic-x@foxmail.com>
2025-05-13 08:14:33 +0800

    feat：二叉树 路径问题
    feat：二叉树 路径问题

--- rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_random/day04/p009/Solution112_01.java
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_random/day04/p009/Solution112_01.java
@@ -11,7 +11,32 @@ public class Solution112_01 {
      * 思路分析：
      */
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        return false;
+        return dfs(root, 0, targetSum);
+    }
+
+    // 递归遍历节点
+    private boolean dfs(TreeNode node, int curPathSum, int targetSum) {
+        if (node == null) {
+            return false;
+        }
+
+        // 处理当前节点
+        curPathSum += node.val;
+
+        // 如果是叶子节点，则判断当前路径值
+        if (node.left == null && node.right == null) {
+            if (curPathSum == targetSum) {
+                return true;
+            }
+        }
+
+        // 处理左节点
+        boolean left = dfs(node.left, curPathSum, targetSum);
+
+        // 处理右节点
+        boolean right = dfs(node.right, curPathSum, targetSum);
+
+        return left || right;
     }
 
 }
