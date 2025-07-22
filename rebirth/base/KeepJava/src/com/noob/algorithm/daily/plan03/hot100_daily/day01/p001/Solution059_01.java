2eea9b59576c456f0d7ed9e48354708f8dd96257
holic-x <holic-x@foxmail.com>
2025-07-22 09:00:15 +0800

    feat：059 螺旋矩阵II
    feat：059 螺旋矩阵II

--- rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_daily/day01/p001/Solution059_01.java
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_daily/day01/p001/Solution059_01.java
@@ -6,9 +6,47 @@ package com.noob.algorithm.daily.plan03.hot100_daily.day01.p001;
  */
 public class Solution059_01 {
     /**
-     * 思路分析：
+     * 思路分析：定义4个方向的指针，分别向4个方向进行遍历并计数
      */
     public int[][] generateMatrix(int n) {
-       return new int[][]{};
+        // 定义目标矩阵
+        int[][] matrix = new int[n][n];
+
+        // 定义4个方向的指针边界
+        int left = 0, right = n - 1, top = 0, bottom = n - 1;
+
+        // 当指针处于有效区域内则进行4个方向的遍历
+        int cnt = 1;
+        while (left <= right && top <= bottom) {
+            // ① 从左往右
+            for (int col = left; col <= right; col++) {
+                matrix[top][col] = cnt++;
+            }
+            top++;
+
+            // ② 从上往下
+            for (int row = top; row <= bottom; row++) {
+                matrix[row][right] = cnt++;
+            }
+            right--;
+
+            // ③ 从右往左
+            if (left <= right) {
+                for (int col = right; col >= left; col--) {
+                    matrix[bottom][col] = cnt++;
+                }
+                bottom--;
+            }
+
+            // ④ 从下往上
+            if (top <= bottom) {
+                for (int row = bottom; row >= top; row--) {
+                    matrix[row][left] = cnt++;
+                }
+                left++;
+            }
+        }
+        // 返回构建的矩阵结果
+        return matrix;
     }
 }
