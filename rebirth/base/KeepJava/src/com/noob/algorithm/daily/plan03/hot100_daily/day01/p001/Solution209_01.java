ecf77b10010813975be615e1f25ed1cd916815f3
holic-x <holic-x@foxmail.com>
2025-07-22 09:00:30 +0800

    feat：209 长度最小的子数组
    feat：209 长度最小的子数组

--- rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_daily/day01/p001/Solution209_01.java
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_daily/day01/p001/Solution209_01.java
@@ -7,9 +7,31 @@ package com.noob.algorithm.daily.plan03.hot100_daily.day01.p001;
 public class Solution209_01 {
 
     /**
-     * 思路分析：
+     * 思路分析：滑动窗口思路
      */
     public int minSubArrayLen(int target, int[] nums) {
-        return 0;
+
+        // 定义滑动窗口的边界指针
+        int left = 0, right = 0;
+
+        int winCnt = 0;
+
+        int minLen = Integer.MAX_VALUE;
+        while (right < nums.length) {
+            // 将元素载入窗口
+            winCnt += nums[right];
+            // 判断当前窗口与target的大小，如果超出目标值则不断移出左侧元素
+            while (winCnt >= target) {
+                // 记录minLen
+                minLen = Math.min(minLen, right - left + 1);
+                // 移除左边界值元素
+                winCnt -= nums[left++];
+            }
+            // 右侧指针处理完成，移动
+            right++;
+        }
+
+        // 返回结果
+        return minLen == Integer.MAX_VALUE ? 0 : minLen;
     }
 }
