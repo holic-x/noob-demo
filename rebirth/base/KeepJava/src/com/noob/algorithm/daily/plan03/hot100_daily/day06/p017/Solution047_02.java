631a8c2a74158d9ed92070781b979bd95e326337
holic-x <holic-x@foxmail.com>
2025-08-05 14:56:44 +0800

    feat：全排列
    feat：全排列

--- rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_daily/day06/p017/Solution047_02.java
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_daily/day06/p017/Solution047_02.java
@@ -7,34 +7,38 @@ import java.util.List;
 /**
  * 🟡 047 全排列II - https://leetcode.cn/problems/permutations-ii/description/
  */
-public class Solution047_01 {
+public class Solution047_02 {
+
 
     /**
      * 思路分析：给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列
-     * 全排列（交换思路） + 去重剪枝
      */
     public List<List<Integer>> permuteUnique(int[] nums) {
         // 数据排序
         Arrays.sort(nums);
         // 调用回溯算法
-        boolean[] visited = new boolean[nums.length];
-        Arrays.fill(visited, false);
-        backTrack(nums, visited);
+        backTrack(nums);
         // 返回结果
         return ans;
     }
 
+
     private List<List<Integer>> ans = new ArrayList<>();
     private List<Integer> path = new ArrayList<>();
 
-    private void backTrack(int[] nums, boolean[] visited) {
+    private void backTrack(int[] nums) {
+
+        // 记录结果
         if (path.size() == nums.length) {
             ans.add(new ArrayList<>(path));
-            return;
         }
 
         // 回溯处理
+        boolean[] visited = new boolean[nums.length];
+        Arrays.fill(visited, false);
+
         for (int i = 0; i < nums.length; i++) {
+            // 校验元素是否已经被选过
             if (visited[i]) {
                 continue;
             }
@@ -42,13 +46,15 @@ public class Solution047_01 {
                 continue;
             }
 
+            visited[i] = true; // 标记这个元素已经被选过
+
             // 回溯处理
             path.add(nums[i]);
-            visited[i] = true;
-            backTrack(nums, visited);
-            path.remove(path.size() - 1);
-            visited[i] = false;
+            backTrack(nums);
+            path.add(path.size() - 1);
         }
+
     }
+
 
 }
