d25d06d10fe1346d57c840346d24678eeee93712
holic-x <holic-x@foxmail.com>
2025-08-11 23:44:34 +0800

    feat：JavaBase
    feat：JavaBase

---
+++ rebirth/base/KeepJava/src/com/noob/base/oop/ExtendDemo.java
@@ -0,0 +1,23 @@
+package com.noob.base.oop;
+
+public class ExtendDemo {
+
+}
+
+/*
+interface A{
+    default void doSth(){
+        System.out.println("A");
+    }
+}
+
+interface B{
+    default void doSth(){
+        System.out.println("B");
+    }
+}
+
+class C implements A,B{
+
+}
+*/
