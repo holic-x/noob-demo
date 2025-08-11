57c9125b04094b1cfe160b0db29a2054e4d1497a
holic-x <holic-x@foxmail.com>
2025-08-11 10:40:36 +0800

    feat：贪心算法基础题型
    feat：贪心算法基础题型

--- rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_daily/day07/p019/Solution1005_01.java
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_daily/day07/p019/Solution1005_01.java
@@ -1,5 +1,7 @@
 package com.noob.algorithm.daily.plan03.hot100_daily.day07.p019;
 
+import java.util.Arrays;
+
 /**
  * 🟢 1005 K次取反后最大化的数组和 - https://leetcode.cn/problems/maximize-sum-of-array-after-k-negations/description/
  */
@@ -7,9 +9,39 @@ public class Solution1005_01 {
 
     /**
      * 思路分析：
+     * 数组存在负数，因此尽可能将负数都取正，且k有盈余的情况下通过一正一负来抵消（选择一个目前的最小正整数来做为处理参考）
      */
     public int largestSumAfterKNegations(int[] nums, int k) {
-        return -1;
+
+        int sum = 0;
+
+        // 对数组元素进行排序，从小到大来进行负数取正操作
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            // 判断是否为负数且k是否有盈余
+            if (nums[i] < 0 && k > 0) {
+                nums[i] *= -1; // 负数取正
+                k--; // k 消耗1次
+            }
+            /*
+            if (k <= 0) {
+                break; // 如果k消耗完成则可提前退出循环处理
+            }
+             */
+            sum += nums[i]; // 计算处理后的累加和
+        }
+
+        // 校验k是否还有次数
+        if (k <= 0) {
+            return sum; // 如果k无剩余则返回结果（处理完成）
+        }
+
+        // 如果k有剩余则继续选择一个目前的最小正数来做抵消
+        Arrays.sort(nums);
+        int minNum = nums[0];
+        // 校验k的奇偶性
+        return (k % 2 == 0) ? sum : sum - 2 * minNum;
+
     }
 
 }
--- rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_daily/day07/p019/Solution455_01.java
+++ rebirth/base/KeepJava/src/com/noob/algorithm/daily/plan03/hot100_daily/day07/p019/Solution455_01.java
@@ -1,5 +1,7 @@
 package com.noob.algorithm.daily.plan03.hot100_daily.day07.p019;
 
+import java.util.Arrays;
+
 /**
  * 🟢 455 分发饼干 - https://leetcode.cn/problems/assign-cookies/description/
  */
@@ -7,9 +9,33 @@ public class Solution455_01 {
 
     /**
      * 思路分析：
+     * 从小到大进行分配，让小胃口的吃小饼干，如果当前饼干小胃口的都吃不下，后面大胃王更加不符合
      */
     public int findContentChildren(int[] g, int[] s) {
+        // 对数据进行排序
+        Arrays.sort(g);
+        Arrays.sort(s);
 
-        return -1;
+        // 定义遍历指针，分别定位当前分发的饼干索引 和 目标孩子
+        int sIdx = 0, gIdx = 0;
+        int sLen = s.length, gLen = g.length;
+
+        int cnt = 0; // 可以吃到饼干的小孩子个数
+
+        // 由于饼干和小朋友个数不同，此处需要验证饼干发放完成（所有饼干都验证完成）或者小孩子验证完成作为退出条件
+        while (sIdx < sLen && gIdx < gLen) {
+            // 判断当前饼干是否符合目前小朋友的胃口
+            if (g[gIdx] <= s[sIdx]) {
+                cnt++;
+                // 指针后移，继续验证下一组
+                sIdx++;
+                gIdx++;
+            } else {
+                // 当前饼干不满足目前小朋友的胃口，且由于胃口是小->大顺序排序的，则后面的小朋友更加不符合要求，因此跳过这块饼干继续下一块饼干的验证
+                sIdx++;
+            }
+        }
+        // 返回统计结果
+        return cnt;
     }
 }
